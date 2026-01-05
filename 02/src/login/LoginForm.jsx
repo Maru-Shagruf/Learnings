047392b798e3987b79171b2543a1b45e503588a6
SHAGRUF <study.marushagruf@.com>
2026-01-05 17:43:50 +0530

    day 3

--- 02/src/login/LoginForm.jsx
+++ 02/src/login/LoginForm.jsx
@@ -1,20 +1,18 @@
 import { useState } from 'react';
 
 function LoginForm() {
-  // The Memory (State)
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
-  // The Bouncer (Validation Logic)
   const handleSubmit = (e) => {
-    e.preventDefault(); // 1. Put the rock on the paper (Stop Refresh)
+    e.preventDefault(); 
 
     // 2. Check ID
     if (password.length < 5) {
-      setErrorMessage("⛔ Password is too short! Need 5 letters.");
+      setErrorMessage(" Password is too short! Need 5 letters.");
     } else {
-      setErrorMessage(""); // Clear error
-      alert("✅ You are allowed in!");
+      setErrorMessage("");
+      alert(" You are allowed in!");
     }
   };
 
@@ -25,14 +23,12 @@ function LoginForm() {
       <form onSubmit={handleSubmit}>
         <label>Password: </label>
         
-        {/* The Mirror (Controlled Input) */}
         <input 
           type="password" 
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
         
-        {/* The Red Sign (Conditional Rendering) */}
         <p style={{ color: 'red' }}>{errorMessage}</p>
 
         <button type="submit">Enter Club</button>
