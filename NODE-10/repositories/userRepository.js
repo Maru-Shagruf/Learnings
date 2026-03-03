9fce5603c3f7506868804e4e6898a7f9504a9bf8
SHAGRUF <study.marushagruf@.com>
2026-03-03 12:53:19 +0530

    node

--- NODE-10/repositories/userRepository.js
+++
@@ -1,9 +0,0 @@
-import User from '../models/User.js';
-
-export const findUserByEmail = async (email) => {
-  return await User.findOne({ email });
-};
-
-export const createNewUser = async (userData) => {
-  return await User.create(userData);
-};
--- NODE-10/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { register } from '../controllers/authController.js';
-
-const router = express.Router();
-
-// Route connects to the Waiter (Controller)
-router.post('/register', register); 
-
-export default router;
