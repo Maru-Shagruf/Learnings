51324fbcf12734d1b16bea13ba673f6187cc0b12
SHAGRUF <study.marushagruf@.com>
2026-03-03 12:42:13 +0530

    Reapply "Reapply "node""
    This reverts commit 9b5a11dc598a9977e1885ada8171efde9ee88fba.

--- NODE-11/utils/catchAsync.js
+++ NODE-11/utils/catchAsync.js
@@ -1,5 +1,5 @@
 export const catchAsync = (fn) => {
   return (req, res, next) => {
-    fn(req, res, next).catch(next); // Passes any error straight to the global handler
+    fn(req, res, next).catch(next); 
   };
 };
--- NODE-9/routes/productRoutes.js
+++ NODE-9/routes/productRoutes.js
@@ -3,7 +3,6 @@ import { createProduct, getProducts } from '../controllers/productController.js'
 import { verifyToken } from '../middleware/auth.js';
 const router = express.Router();
 
-// Products are PROTECTED! You must pass the verifyToken bouncer.
 router.post('/', verifyToken, createProduct);
 router.get('/', verifyToken, getProducts);
 export default router;
