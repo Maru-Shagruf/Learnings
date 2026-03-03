df011129839e236d3fcd3ae98d20be97a9584d43
SHAGRUF <study.marushagruf@.com>
2026-03-03 12:52:51 +0530

    node

--- NODE-9/models/Product.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } 
-});
-export default mongoose.model('Product', productSchema);
--- NODE-9/routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createProduct, getProducts } from '../controllers/productController.js';
-import { verifyToken } from '../middleware/auth.js';
-const router = express.Router();
-
-router.post('/', verifyToken, createProduct);
-router.get('/', verifyToken, getProducts);
-export default router;
