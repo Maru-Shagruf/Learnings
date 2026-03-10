cc94c735cc7c53b93f9c87928d90d33a8e3a03aa
SHAGRUF <study.marushagruf@.com>
2026-03-10 11:44:30 +0530

    node

--- NODE-18/server.js
+++ NODE-18/server.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 
 //  3. CACHING SETUP 
 
-const redisClient = createClient({ url: '' });
+const redisClient = createClient({ url: '' });// redir url here
 await redisClient.connect();
 
 
